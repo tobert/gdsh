680222787febc6fc3aa56c88ca4150f4e0063e2a
Al Tobey <tobert@gmail.com>
2013-03-04 05:25:37 +0000

    Working sendfile.

--- cmd_sendfile.go
+++ cmd_sendfile.go
@@ -4,6 +4,8 @@ import (
 	"./src/gdssh"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 )
 
 type sfTask struct {
@@ -14,19 +16,39 @@ type sfTask struct {
 func parseSfOptions(opt GdshOptions) *sfTask {
 	task := sfTask{}
 
+	// bare argument style, e.g. gdsh sf /etc/hosts /etc/hosts
 	if len(opt.Args) == 2 {
+		if strings.HasPrefix(opt.Args[0], "-") {
+			log.Fatal("Malformed command? Leading dashes are not allowed with bare arguments.")
+		}
+
+		if strings.HasPrefix(opt.Args[1], "-") {
+			log.Fatal("Malformed command? Leading dashes are not allowed with bare arguments.")
+		}
+
+		_, err := os.Stat(opt.Args[0])
+		if err != nil {
+			log.Fatal(fmt.Sprintf("Not enough arguments. '%s' is not a file: %s", opt.Args[0], err))
+		}
+
 		task.local = opt.Args[0]
 		task.remote = opt.Args[1]
+
+		// fully-specified style, e.g. gdsh scp --local /etc/hosts --remote /etc/hosts
 	} else if len(opt.Args) == 4 {
 		skip := false
 		for i, arg := range opt.Args {
 			if skip {
 				skip = false
 				continue
-			} else if arg == "--local" {
+			} else if arg == "--local" || arg == "-L" {
+				_, err := os.Stat(opt.Args[i+1])
+				if err != nil {
+					log.Fatal(fmt.Sprintf("Invalid local file '%s': %s", opt.Args[i+1], err))
+				}
 				task.local = opt.Args[i+1]
 				skip = true
-			} else if arg == "--remote" {
+			} else if arg == "--remote" || arg == "-R" {
 				task.remote = opt.Args[i+1]
 				skip = true
 			}
@@ -38,26 +60,15 @@ func parseSfOptions(opt GdshOptions) *sfTask {
 	return &task
 }
 
+// the file will be opened for each remote host, but that's fine since
+// the reads will end up shared on modern operating systems
 func (task *sfTask) Run(conn *gdssh.Conn) error {
-	// stat localFile, check for errors
-	// copy localfile
-	//conn.Scp(task.script.Bytes(), "0555", task.filename)
-	fmt.Printf("scp %s %s:%s\n", task.local, conn.Host, task.remote)
+	conn.Scp(task.local, task.remote)
 	return nil
 }
 
 func cmdSendfile(opt GdshOptions) int {
-	//stat, err := os.Stat(opt.)
 	pool := sshPool(opt)
-
-	/*	f, err := os.Open(opt.Script)
-		if err != nil {
-			log.Fatal("Could not read script file '", opt.Script, "': ", err)
-		}
-		io.Copy(run.script, f)
-		f.Close()
-	} */
-
 	task := parseSfOptions(opt)
 	pool.All(task)
 	pool.Close()
