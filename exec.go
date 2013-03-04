ce9a090cb899acc5afd3850dc11c3d2d6fe566cb
Al Tobey <tobert@gmail.com>
2013-03-04 10:37:26 +0000

    remove dead code

--- exec.go
+++ exec.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"log"
 	"os"
-	"strings"
 	"syscall"
 )
 
 func execLocalCommand(command string, args []string) (err error) {
-	log.Printf("COMMAND: '%s %s'\n", command, strings.Join(args, " "))
-
 	// the exec syscall expects the command name to be in argv
 	argv := make([]string, len(args)+1)
 	argv[0] = command
@@ -18,10 +14,6 @@ func execLocalCommand(command string, args []string) (err error) {
 	}
 
 	return syscall.Exec(command, argv, os.Environ())
-}
-
-func runRemoteCommand(command []string) (err error) {
-	return nil
 }
 
 // vim: ts=4 sw=4 noet tw=120 softtabstop=4
